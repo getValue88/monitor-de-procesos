90f894b99ecb428a097018c012ef1c679fd2abee
Francisco Corti <fc_pincharrata@hotmail.com.ar>
2019-11-14 02:53:34 -0300

    added user/login endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    public constructor(private readonly userService: UserService) { }
+
+    @Post('/login')
+    login(@Body() uData:any) {
+        return this.userService.login(uData);
+    }
+
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, QueryBuilder } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    public constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) { }
+
+    public async login(uData: any): Promise<any> {
+        let uFound = await this.userRepository.findOne({ where: { name: uData['username'] } });
+        if (uFound) {
+            if (uData['password'] === uFound.getPassword()) {
+                return {
+                    'id': `${uFound.getID()}`,
+                    'username': `${uFound.getName()}`,
+                    'privilege': `${uFound.getPrivilege()}`
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    }
+}
