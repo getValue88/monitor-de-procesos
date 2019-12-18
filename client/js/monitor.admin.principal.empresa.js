d0404dbaedfbffbe4b9b400e458bd5364415036c
chaveta <sebastianestebanchaves@gmail.com>
2019-12-18 15:32:23 -0300

    monitor.admin.principal.empresa visual enhancing and code depuration

--- client/js/monitor.admin.principal.empresa.js
+++ client/js/monitor.admin.principal.empresa.js
@@ -1,8 +1,6 @@
-// Leo el parámetro que viene del llamado de la página anterior
-// para obtener el userId del admin.
+// Leo el parámetro que viene del llamado de la página anterior para obtener el userId del admin.
 // Se accede al mismo con las sentencia: 
 // params['userId']
-
 let paramstr = window.location.search.substr(1);
 let paramarr = paramstr.split("&");
 let params = [];
@@ -11,51 +9,47 @@ for (let i = 0; i < paramarr.length; i++) {
     params[tmparr[0]] = tmparr[1];
 }
 let userId = params['userId'];
+
+// Declaro variables
 let companyID;
 
-// Botón Guardar
+// Asigno su listener al botón Guardar
 let btnGuardar = document.querySelector("#btnGuardar");
 btnGuardar.addEventListener("click", guardar);
 
-// Botón Volver
+// Asigno su listener al botón Volver
 let btnVolver = document.querySelector("#btnVolver");
 btnVolver.addEventListener("click", volver);
 
-// Se llama a la función que actualiza el formulario
+// Llamo a la función que actualiza el formulario
 load(userId);
 
-// Función que solicita al servidor los datos de la empresa
+// Función que actualiza el formulario con los datos existentes
 async function load(userId) {
     try {
         // Busco la compania que corresponde al userId
         let response = await fetch(`../user/company/${userId}`);
         let respuesta = await response.json();
         companyID = respuesta['id'];
-        // Debug
-        console.log("userId: " + userId + " companyId: " + companyID);
         // Se cargan los campos del formulario con los datos recibidos
         document.querySelector('#rs').value = respuesta['rs'];
         document.querySelector('#address').value = respuesta['address'];
         document.querySelector('#impositiveCategory').value = respuesta['impositiveCategory'];
         document.querySelector('#cuit').value = respuesta['cuit'];
         document.querySelector('#logo').value = respuesta['logo'];
-
     } catch (err) {
         alert(err.message);
     }
-    
 }
 
 // Función que guarda los datos de la empresa
 async function guardar() {
-    
     // Obtengo los datos del DOM
     let rs = document.querySelector('#rs').value;
     let address = document.querySelector('#address').value;
     let impositiveCategory = document.querySelector('#impositiveCategory').value;
     let cuit = document.querySelector('#cuit').value;
     let logo = document.querySelector('#logo').value;
-
     // Armo un registro con los datos obtenidos
     let registro = {
         "rs": rs,
@@ -64,7 +58,6 @@ async function guardar() {
         "cuit": cuit,
         "logo": logo
     }
-
     // Solicito el PUT al servidor
     let response = await fetch(`../company/${companyID}`, {
         "method": "PUT",
@@ -73,11 +66,11 @@ async function guardar() {
         },
         "body": JSON.stringify(registro)
     })
-
-    // Vuelve a la página principal
+    // Vuelvo a la página anterior
     location.href = `/html/monitor.admin.principal.html?userId=${userId}`;
 }
 
+// Función que vuelve a la página anterior
 function volver() {
     location.href = `/html/monitor.admin.principal.html?userId=${userId}`;
 }
