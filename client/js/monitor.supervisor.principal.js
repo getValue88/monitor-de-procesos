c308d1cd0375069e7d9d0afd00c1aa33b36cd8b1
chaveta <sebastianestebanchaves@gmail.com>
2019-12-18 12:01:58 -0300

    monitor.supervisor.principal visual enhancing

--- client/js/monitor.supervisor.principal.js
+++ client/js/monitor.supervisor.principal.js
@@ -1,5 +1,4 @@
-// Leo el parámetro que viene del llamado de la página anterior
-// para obtener el userId del admin.
+// Leo el parámetro que viene del llamado de la página anterior para obtener el userId del admin.
 // Se accede al mismo con la sentencia: params['userId']
 
 let paramstr = window.location.search.substr(1);
@@ -11,18 +10,20 @@ for (let i = 0; i < paramarr.length; i++) {
 }
 let userId = params['userId'];
 
-// Botón Ordenes de Fabricación
+// Asigno su listener al botón Ordenes de Fabricación
 let btnOrdenesFabricacion = document.querySelector("#btnOrdenesFabricacion");
 btnOrdenesFabricacion.addEventListener("click", ordenesFabricacion);
  
-// Botón Cerrar Sesión
+// Asigno su listener al botón Cerrar Sesión
 let btnCerrarSesion = document.querySelector("#btnCerrarSesion");
 btnCerrarSesion.addEventListener("click", cerrarSesion);
 
+// Función que llama a la vista ordenes de fabricación
 function ordenesFabricacion(){
     location.href = `/html/monitor.supervisor.principal.ordenesfabricacion.html?userId=${userId}`; 
 }
 
+// Función que cierra la sesión del supervisor
 function cerrarSesion(){
     location.href = `/monitor.index.html`;
 }
